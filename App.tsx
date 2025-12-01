c3c9952c7fb4fa5e757eaa81cc88c653996cd083
Nicolas Descamps <nicolas.descamps@theodo.com>
2025-12-01 16:02:21 +0100

    fix: update dojo

--- App.tsx
+++ App.tsx
@@ -3,7 +3,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello ENSIMAG! 🐘</Text>
+      <Text>Hello ! 👋</Text>
     </View>
   );
 }
